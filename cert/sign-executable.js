e676c2a3655a8705a07b68337d8e5acd9cf55067
Norman Huth <norman@huth.it>
2023-09-25 22:13:43 +0200

    chore: change code signing cryptographic algorithm

--- cert/sign-executable.js
+++ cert/sign-executable.js
@@ -4,7 +4,7 @@ exports.default = async function (context) {
   if (process.platform === 'win32') {
     let command = 'cd ' + context.appOutDir
     command += ' && signtool sign /n "Open Source Developer, Norman Huth"'
-    command += ' /t http://time.certum.pl/ /fd sha1 /v "'
+    command += ' /t http://time.certum.pl/ /fd sha256 /v "'
     command += context.packager.appInfo.productFilename + '.exe"'
     exec(command, function (error, stdout) {
       if (error) {
--- cert/sign-setup.js
+++ cert/sign-setup.js
@@ -4,7 +4,7 @@ exports.default = function (data) {
   if (process.platform === 'win32') {
     let artifactPaths = data.artifactPaths.filter((path) => path.endsWith('.exe'))
     let command = 'signtool sign /n "Open Source Developer, Norman Huth"'
-    command += ' /t http://time.certum.pl/ /fd sha1 /v '
+    command += ' /t http://time.certum.pl/ /fd sha256 /v '
     command += '"' + artifactPaths[0] + '"'
     exec(command, function (error, stdout) {
       if (error) {
